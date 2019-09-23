6f55a766bd821709c66e70d8da2897cd7b28f0e7
unbrace3 <unbrace3@gmail.com>
2019-09-23 02:46:57 -0400

    add deep gambler code

--- models/deepgambler.py
+++ models/deepgambler.py
@@ -22,9 +22,10 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4,
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-class SelectiveNet(nn.Module):
-    def __init__(self):
-        super(SelectiveNet, self).__init__()
+class DeepGambler(nn.Module):
+    def __init__(self, payoff):
+        super(DeepGambler, self).__init__()
+        self.payoff = payoff
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -32,14 +33,9 @@ class SelectiveNet(nn.Module):
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 42)
         
-        self.classification_head1 = nn.Linear(42, 10)
+        self.classification = nn.Linear(42, 10)
+        self.abstain = nn.Linear(42, 1)
         
-        self.selection_head1 = nn.Linear(42, 42)
-        self.selection_head2 = nn.Linear(42, 1)
-        self.selection_head3 = nn.Sigmoid()
-        
-        self.auxiliary_head1 = nn.Linear(42, 10)
-
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
@@ -47,26 +43,19 @@ class SelectiveNet(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        # calculate classification head
-        f = self.classification_head1(x)
-        # calculate selection head
-        g = F.relu(self.selection_head1(x))
-        g = self.selection_head2(g)
-        g = self.selection_head3(g)
-        # calculate auxiliary head              
-        h = self.auxiliary_head1(x)
-        return (f, g, h)
+        
+        f = self.classification(x)
+        g = self.abstain(x)
+        
+        return (f * self.payoff) + g
 
-snet = SelectiveNet()
-criterion1 = nn.CrossEntropyLoss()
-criterion2 = nn.CrossEntropyLoss()
-optimizer = optim.SGD(snet.parameters(), lr=0.001, momentum=0.9)
+payoffs = torch.tensor(7.0, requires_grad=False)
+dgambler = DeepGambler(payoffs)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(dgambler.parameters(), lr=0.001, momentum=0.9)
 
 def train():
     running_loss = 0.0
-    alpha = 0.5
-    lambd = 32
-    c = 0.8
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
@@ -75,13 +64,8 @@ def train():
         optimizer.zero_grad()
 
         # forward + backward + optimize
-        f, g, h = snet(inputs)
-        coverage = torch.mean(g)
-        risk = torch.mean(criterion1(f, labels) * g) / coverage
-        fgLoss = risk + lambd * (torch.max(c - coverage, 0))[0].pow(2)
-        hLoss = criterion2(h, labels)
-        
-        loss = alpha * fgLoss + (1 - alpha) * hLoss
+        outputs = dgambler(inputs)
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
 
